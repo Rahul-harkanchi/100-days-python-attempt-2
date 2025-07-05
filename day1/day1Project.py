32e4753223d21efa7b59a29e3d952d2de798a5e4
Rahul Harkanchi <oriel.rahul@gmail.com>
2025-07-05 10:22:38 +0530

    first attempt to upload work through terminal

---
+++ day1/day1Project.py
@@ -0,0 +1,4 @@
+print("well come to band name generator")
+city = input("whats the name of the city you grew up i\n")
+pet = input("what is the name of your pet?\n")
+print("your band name could be: "+ city +" " + pet)
--- day1/print.py
+++ day1/print.py
@@ -1,1 +1,34 @@
 print("hello world!")
+
+
+#string manipulation and code intelligence
+#using \n to add new line in a print statement
+print("hello world! \nhello world")
+
+#string concatenation
+print("Hello" + "  Angela")
+
+# task 3
+# input function, creating input functions and using them
+input("what is your name")
+
+# writing a programming using input function
+print("hello" + input("what is your name "))
+
+# variables in python
+
+name = input("what is your name ")
+print(name)
+
+# calculating the length of the variable using len() function
+print(len(name))
+
+# variable naming rules
+
+# Make sure your variable names are descriptive
+# Don't have spaces between words
+# Don't start with numbers
+# Don't use special words like print or input
+# Choose simple words that are less likely to become typos
+# Check the company style guidelines if you start work at a company
+
